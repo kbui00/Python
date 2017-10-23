bc9bee9935eb17bc6cfd183ea8ad14eca5b74f78
kbui00 <32948131+kbui00@users.noreply.github.com>
2017-10-23 07:23:08 -0500

    Update Pig Latin

---
+++ CodeCademy/Pig Latin.py
@@ -0,0 +1,12 @@
+import os
+os.system('cls')
+
+str_input = input('Enter a word: ')
+while not str_input.isalpha():
+    str_input = input('Please enter a word: ')
+word = str_input.lower()
+firstword = word[0]
+aytail = 'ay.'
+translatedword = word.replace(word[0],"")
+translatedword = translatedword + firstword + aytail
+print (translatedword)
