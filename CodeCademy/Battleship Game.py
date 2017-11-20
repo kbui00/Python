79cb0b5f2b607399da4e9af5d4cb098c87abe87d
kbui00 <32948131+kbui00@users.noreply.github.com>
2017-11-20 10:07:27 -0600

    Update with size input

--- CodeCademy/Battleship Game.py
+++ CodeCademy/Battleship Game.py
@@ -4,7 +4,8 @@ board = []
 x_axis = []
 y_axis = []
 
-
+#size = int(raw_input('Enter board size: ')
+size = eval(input('Enter board size: '))
 
 for x in range(0,size):
   board.append(["O"] * size)
@@ -28,7 +29,7 @@ ship_col = random_col(board)
 print (ship_row)
 print (ship_col)
 
-for turn in range(4):
+for turn in range(size-1):
   print ("Turn", turn + 1)
   #guess_row = int(raw_input("Guess Row: "))        #Python 2
   #guess_col = int(raw_input("Guess Col: "))        #Python 2
@@ -39,7 +40,7 @@ for turn in range(4):
     print ("Congratulations! You sank my battleship!")
     break
   else:
-    if guess_row not in range(5) or guess_col not in range(5):
+    if guess_row not in range(size) or guess_col not in range(size):
       print ("Oops, that's not even in the ocean.")
     elif board[guess_row][guess_col] == "X":
       print( "You guessed that one already." )
@@ -47,5 +48,5 @@ for turn in range(4):
       print ("You missed my battleship!")
       board[guess_row][guess_col] = "X"
     print_board(board)
-    if turn == 3:
+    if turn == size - 2:
       print ('Game Over')
